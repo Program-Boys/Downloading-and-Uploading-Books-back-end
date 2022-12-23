e245416232c0efa10cce399a8940a97449aa3db4
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-12-23 15:02:21 -0300

    feat: Adding-cors-and-file-errors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import { handleErrors } from './errors/handleError';
 import routes from './routes/books.routes';
 import routesUser from './routes/user.routes';
 
-const app = express();
-app.use(express.json());
+let cors = require('cors');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(handleErrors);
 app.use('/books', routes);
 app.use('/user', routesUser);
 
