353a2660b8a0424f1ec7466733770fbeccabb131
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-12-23 16:03:13 -0300

    feat: function-list-user-in-controller

--- src/controllers/User/User.controllers.ts
+++ src/controllers/User/User.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import createUserService from '../../services/User/createUser.service';
+import listUserService from '../../services/User/listUser.service';
 
 export class UserController {
   async create(req: Request, res: Response) {
@@ -8,5 +9,11 @@ export class UserController {
     const createUser = await createUserService({ email, password });
 
     return res.status(201).json(createUser);
+  }
+
+  async list(req: Request, res: Response) {
+    const listUser = await listUserService();
+
+    return res.json(listUser);
   }
 }
