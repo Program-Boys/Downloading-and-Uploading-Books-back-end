66a9e0a8c47ed1faa0eb841b98bdb9e3ab1b8794
Enrique <enrique.barbosasilva@gmail.com>
2022-12-23 16:05:16 -0300

    refactor: new-route-added

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -1,9 +1,10 @@
-import { Router } from "express";
-import { BooksControllers } from "../controllers/Books/createBook.controller";
+import { Router } from 'express';
+import { BooksControllers } from '../controllers/Books/createBook.controller';
 
 const routes = Router();
 const booksControllers = new BooksControllers();
 
-routes.post("", booksControllers.create);
+routes.post('', booksControllers.create);
+routes.get('', booksControllers.list);
 
 export default routes;
