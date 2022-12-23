87b5f87dd7aab829aec847f4be02336e26563e7b
Enrique <enrique.barbosasilva@gmail.com>
2022-12-23 16:03:52 -0300

    refactor: code-changed

--- src/controllers/Books/createBook.controller.ts
+++ src/controllers/Books/createBook.controller.ts
@@ -1,4 +1,5 @@
 import createBookService from '../../services/Books/createBook.service';
+import listBooksService from '../../services/Books/listBooks.service';
 import { Request, Response } from 'express';
 
 export class BooksControllers {
@@ -7,5 +8,11 @@ export class BooksControllers {
 
     const createdBook = await createBookService({ name, gender });
     return res.status(201).json(createdBook);
+  }
+
+  async list(req: Request, res: Response) {
+    const books = await listBooksService();
+
+    return res.status(200).json(books);
   }
 }
