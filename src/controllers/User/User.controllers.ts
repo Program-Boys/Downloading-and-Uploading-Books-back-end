672ceb4d5e12d79ae66718029a2180e3b9432343
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-12-23 15:03:50 -0300

    refactory: removing-try-catch-statement

--- src/controllers/User/User.controllers.ts
+++ src/controllers/User/User.controllers.ts
@@ -3,16 +3,10 @@ import createUserService from '../../services/User/createUser.service';
 
 export class UserController {
   async create(req: Request, res: Response) {
-    try {
-      const { email, password } = req.body;
+    const { email, password } = req.body;
 
-      const createUser = await createUserService({ email, password });
+    const createUser = await createUserService({ email, password });
 
-      return res.status(201).json(createUser);
-    } catch (error) {
-      return res.status(400).json({
-        error: 'You are not admin',
-      });
-    }
+    return res.status(201).json(createUser);
   }
 }
