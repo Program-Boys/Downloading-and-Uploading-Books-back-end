8395a282e332a318e75ac9aa5c707481a4c12b33
Enrique <enrique.barbosasilva@gmail.com>
2022-12-23 00:19:57 -0300

    refactor: code-changed

--- src/controllers/Books/createBook.controller.ts
+++ src/controllers/Books/createBook.controller.ts
@@ -1,11 +1,17 @@
-import createBookService from "../../services/Books/createBook.service";
-import { Request, Response } from "express";
+import createBookService from '../../services/Books/createBook.service';
+import { Request, Response } from 'express';
 
 export class BooksControllers {
   async create(req: Request, res: Response) {
-    const { name, gender } = req.body;
+    try {
+      const { name, gender } = req.body;
 
-    const createdBook = await createBookService({ name, gender });
-    return res.status(201).json(createdBook);
+      const createdBook = await createBookService({ name, gender });
+      return res.status(201).json(createdBook);
+    } catch (error) {
+      return res.status(400).json({
+        error: 'You are not admin',
+      });
+    }
   }
 }
