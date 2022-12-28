5111b5971247500cdee2c34761895aad28420626
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-12-28 15:20:28 -0300

    feat: delete-function

--- src/controllers/Books/Book.controller.ts
+++ src/controllers/Books/Book.controller.ts
@@ -1,6 +1,7 @@
 import createBookService from '../../services/Books/createBook.service';
 import listBooksService from '../../services/Books/listBooks.service';
 import { Request, Response } from 'express';
+import deleteBookService from '../../services/Books/deleteBook.service';
 
 export class BooksControllers {
   async create(req: Request, res: Response) {
@@ -17,5 +18,13 @@ export class BooksControllers {
     const books = await listBooksService();
 
     return res.status(200).json(books);
+  }
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    await deleteBookService({ id });
+
+    return res.status(204);
   }
 }
