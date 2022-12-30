c999fc292badd5b9ef480c95571dae1fe6329b30
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-12-30 15:24:39 -0300

    feat: list-one-by-id-and-patch-functions

--- src/controllers/Books/Book.controller.ts
+++ src/controllers/Books/Book.controller.ts
@@ -2,6 +2,8 @@ import createBookService from '../../services/Books/createBook.service';
 import listBooksService from '../../services/Books/listBooks.service';
 import deleteBookService from '../../services/Books/deleteBook.service';
 import { Request, Response } from 'express';
+import listOneBookService from '../../services/Books/listOneBook.service';
+import patchBookFileService from '../../services/Books/patchBookFile.service';
 
 export class BooksControllers {
   async create(req: Request, res: Response) {
@@ -11,6 +13,7 @@ export class BooksControllers {
       name,
       gender,
     });
+
     return res.status(201).json(createdBook);
   }
 
@@ -18,6 +21,24 @@ export class BooksControllers {
     const books = await listBooksService();
 
     return res.status(200).json(books);
+  }
+
+  async listOneById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const oneBook = await listOneBookService({ id });
+
+    return res.status(200).json(oneBook);
+  }
+
+  async patchBookFile(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const bookPatched = await patchBookFileService(req, { id });
+
+    const nada = req.file;
+
+    console.log(nada);
   }
 
   async delete(req: Request, res: Response) {
