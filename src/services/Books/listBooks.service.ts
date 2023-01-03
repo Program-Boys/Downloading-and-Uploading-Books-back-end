eca4e66703dd0b7fbd080514fe94d4450f9c1466
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2023-01-03 01:21:46 -0300

    refactor: changing-exit-for-listing

--- src/services/Books/listBooks.service.ts
+++ src/services/Books/listBooks.service.ts
@@ -2,7 +2,11 @@ import { Books } from '@prisma/client';
 import { prisma } from '../../../prisma/client/client';
 
 const listBooksService = async (): Promise<Books[]> => {
-  const books = await prisma.books.findMany();
+  const books = await prisma.books.findMany({
+    include: {
+      bookFile: true,
+    },
+  });
 
   return books;
 };
