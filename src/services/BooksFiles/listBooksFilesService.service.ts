48fbe7da45a79eda9f731730156954ada6e62be5
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2023-01-03 01:22:20 -0300

    feat: list-bookFiles-function-service

--- src/services/BooksFiles/listBooksFilesService.service.ts
+++ src/services/BooksFiles/listBooksFilesService.service.ts
@@ -3,14 +3,13 @@ import { prisma } from '../../../prisma/client/client';
 import { BooksFilesExit } from '../../interfaces/booksFiles';
 
 const listBooksFilesService = async (): Promise<BooksFilesExit[]> => {
-  const fileBooks = await prisma.bookFile.findMany();
+  const fileBooks = await prisma.bookFile.findMany({});
 
   const newExit = fileBooks.map((file) => {
     return {
       id: file?.id,
       originalName: file?.originalName,
       filename: file?.filename,
-      path: file?.path,
     };
   });
 
