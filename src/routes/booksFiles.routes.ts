ed6440213785809d658011d0967f690ed66d0a44
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2023-01-04 14:00:34 -0300

    feat: route-for-listing-and-patching-bookFiles

--- src/routes/booksFiles.routes.ts
+++ src/routes/booksFiles.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { BooksFilesController } from '../controllers/BooksFiles/booksFiles.controller';
+import { storage } from '../multerConfig';
 
+const upload = multer({ storage: storage });
 const routes = Router();
 const booksFilesController = new BooksFilesController();
 
 routes.get('', booksFilesController.list);
+routes.patch('/:id', upload.single('Books'), booksFilesController.updateFile);
 
 export default routes;
