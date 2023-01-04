7070520add8cb9e3644e2e636dcea5fac0407497
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2023-01-04 14:02:49 -0300

    feat: new-BooksFilesController

--- src/controllers/BooksFiles/booksFiles.controller.ts
+++ src/controllers/BooksFiles/booksFiles.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import patchBookFileService from '../../services/Books/patchBookFile.service';
 import listBooksFilesService from '../../services/BooksFiles/listBooksFilesService.service';
 
 export class BooksFilesController {
@@ -6,5 +7,17 @@ export class BooksFilesController {
     const booksFiles = await listBooksFilesService();
 
     return res.status(200).json(booksFiles);
+  }
+
+  async updateFile(req: Request, res: Response) {
+    const { id } = req.params;
+    const { originalName, filename } = req.body;
+    const updatedBookFiles = await patchBookFileService(req, {
+      originalName,
+      filename,
+      id,
+    });
+
+    return res.status(202).json(updatedBookFiles);
   }
 }
