ee32e6dbebc160de04a54f628e108a2aa3d1a752
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2023-01-05 16:06:42 -0300

    feat: new-function-patch

--- src/controllers/Books/Book.controller.ts
+++ src/controllers/Books/Book.controller.ts
@@ -3,6 +3,7 @@ import listBooksService from '../../services/Books/listBooks.service';
 import deleteBookService from '../../services/Books/deleteBook.service';
 import { Request, Response } from 'express';
 import listOneBookService from '../../services/Books/listOneBook.service';
+import patchBookService from '../../services/Books/patchBook.service';
 
 export class BooksControllers {
   async create(req: Request, res: Response) {
@@ -28,6 +29,15 @@ export class BooksControllers {
     const oneBook = await listOneBookService({ id });
 
     return res.status(200).json(oneBook);
+  }
+
+  async patch(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, gender } = req.body;
+
+    const patchedBook = await patchBookService({ id, name, gender });
+
+    return res.status(202).json(patchedBook);
   }
 
   async delete(req: Request, res: Response) {
