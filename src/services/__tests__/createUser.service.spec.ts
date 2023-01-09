33d3e10090068614c8c0f8bd314e402281205259
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2023-01-09 18:45:48 -0300

    feat: test-to-create-a-user

--- src/services/__tests__/createUser.service.spec.ts
+++ src/services/__tests__/createUser.service.spec.ts
@@ -1,11 +1,9 @@
-import { Prisma } from '@prisma/client';
-import { AppError } from '../../errors/AppError';
 import { InMemoryUserRepository } from '../../models/user.model';
 import createUserService from '../User/createUser.service';
 
-const userRepository = new InMemoryUserRepository();
-
 describe('Create a User', () => {
+  const userRepository = new InMemoryUserRepository();
+
   it('Should be able to create a user', async () => {
     const email = 'angela@gmail.com';
     const password = '12345678';
@@ -14,19 +12,23 @@ describe('Create a User', () => {
 
     await userRepository.create(user);
 
-    expect(userRepository.userInMemory).toBeTruthy();
+    expect(userRepository.userInMemory[0]).toBe(user);
   });
 
   it('Should not be able to create a user', async () => {
     const email = 'guiteste@gmail.com';
     const password = '12345678';
 
-    const user = await createUserService({ email, password });
-
-    await userRepository.create(user);
-
-    expect(userRepository.userInMemory).rejects.toThrowError(
-      new AppError(400, 'You are not admin'),
+    expect(
+      await createUserService({ email, password })
+        .then()
+        .catch((_) => _),
     );
+  });
+
+  it('Should be able to list the users', async () => {
+    const users = await userRepository.list();
+
+    expect(users.length).toBeGreaterThan(0);
   });
 });
