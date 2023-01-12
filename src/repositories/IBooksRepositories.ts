a419bfad2901e73117104ee8a6aa5cf928c1ffd4
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2023-01-12 18:48:56 -0300

    feat: adding-new-function-in-interface

--- src/repositories/IBooksRepositories.ts
+++ src/repositories/IBooksRepositories.ts
@@ -3,5 +3,6 @@ import { Book } from '../entities/book';
 interface IBooksRepository {
   create(book: Book): Promise<Book>;
   list(books: Book[]): Promise<Book[]>;
+  listOne(id: Book): Promise<Book>;
 }
 export { IBooksRepository };
