c0de7ee8edfc102759dbeeac8e97c6be8b7bc514
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2023-01-13 19:02:16 -0300

    feat: using-middleware-in-routes-get-and-patch

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -3,6 +3,7 @@ import { BooksControllers } from '../controllers/Books/Book.controller';
 import { storage } from '../multerConfig';
 import multer from 'multer';
 import tokenAuthMiddleware from '../middlewares/tokenAuth.middleware';
+import verifyNameGenderLength from '../middlewares/verifyNameGenderLength.middleware';
 
 const upload = multer({ storage: storage });
 
@@ -12,12 +13,13 @@ const booksControllers = new BooksControllers();
 routes.post(
   '',
   upload.single('Books'),
+  verifyNameGenderLength,
   tokenAuthMiddleware,
   booksControllers.create,
 );
 routes.get('', booksControllers.list);
 routes.get('/:id', booksControllers.listOneById);
-routes.patch('/:id', booksControllers.patch);
+routes.patch('/:id', verifyNameGenderLength, booksControllers.patch);
 routes.delete('/:id', tokenAuthMiddleware, booksControllers.delete);
 
 export default routes;
